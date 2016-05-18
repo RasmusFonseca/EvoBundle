1b58eee5adc04a996e70e4e3a8de6c0279b5a803
Rasmus Fonseca <fonseca.rasmus@gmail.com>
2016-05-18 11:57:57 -0700

    Almost working prototype

--- packages.js
+++ packages.js
@@ -1,49 +1,56 @@
-(function() {
-  packages = {
+genRoot = function(classes) {
+  var map = {};
 
-    // Lazily construct the package hierarchy from class names.
-    root: function(classes) {
-      var map = {};
-
-      function find(name, data) {
-        var node = map[name], i;
-        if (!node) {
-          node = map[name] = data || {name: name, children: []};
-          if (name.length) {
-            node.parent = find(name.substring(0, i = name.lastIndexOf(".")));
-            node.parent.children.push(node);
-            node.key = name.substring(i + 1);
-          }
-        }
-        return node;
+  function find(name, data) {
+    var node = map[name], i;
+    if (!node) {
+      node = map[name] = data || {name: name, children: []};
+      if (name.length) {
+        node.parent = find(name.substring(0, i = name.lastIndexOf(".")));
+        node.parent.children.push(node);
+        node.key = name.substring(i + 1);
       }
-
-      classes.forEach(function(d) {
-        find(d.name, d);
-      });
-
-      return map[""];
-    },
-
-    // Return a list of imports for the given array of nodes.
-    imports: function(nodes) {
-      var map = {},
-          imports = [];
-
-      // Compute a map from name to node.
-      nodes.forEach(function(d) {
-        map[d.name] = d;
-      });
-
-      // For each import, construct a link from the source to target node.
-      nodes.forEach(function(d) {
-        if (d.imports) d.imports.forEach(function(i) {
-          imports.push({source: map[d.name], target: map[i]});
-        });
-      });
-
-      return imports;
     }
-
+    return node;
   };
-})();
+
+  lblDefs = classes.filter(function(d){ return d.name!=undefined; });
+  lblDefs.forEach(function(d){
+    find(d.name, d);
+  });
+  console.log(map[""])
+
+  return map[""];
+}
+
+// Return a list of imports for the given array of nodes.
+genLinks = function(nodes) {
+  var map = {},
+  imports = [];
+
+  // Compute a map from name to node.
+  nodes.filter(function(d){ return d.name!=undefined; }).forEach(function(d) {
+    map[d.name.substring(d.name.lastIndexOf(".")+1)] = d;
+  });
+
+  // For each import, construct a link from the source to target node.
+  nodes.filter(function(d){ return d.name==undefined; }).forEach(function(d) {
+    while(imports.length<d.edgeEvo.length) 
+      imports.push([]);
+
+    d.edgeEvo.forEach(
+        function(connected,i){ 
+          if(connected==1) 
+            imports[i].push({source: map[d.name1], target: map[d.name2]}); 
+        } 
+    );
+
+      
+    //if (d.edgeEvo[0]==1){
+    //  imports.push({source: map[d.name1], target: map[d.name2]});
+    //}
+  });
+
+  console.log(imports);
+  return imports;
+}
