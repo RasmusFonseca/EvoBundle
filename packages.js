ba6130e728eb908c0af21f6eeea66f974ec836c8
Rasmus Fonseca <fonseca.rasmus@gmail.com>
2016-05-18 20:15:06 -0700

    Compressed input format works. No more edge weights.

--- packages.js
+++ packages.js
@@ -51,7 +51,7 @@ genLinks = function(nodes) {
   return imports;
 }
 
-function csvToRoot(data){
+function csvToTree(data){
   var map = {};
 
   function find(name, data) {
@@ -67,62 +67,47 @@ function csvToRoot(data){
     return node;
   };
 
-  var lblDefs = data
-      .filter(function(d){ return d.length==1; })
-      .map(function(d){ return d[0]; });
-
-  if(lblDefs.length==0){
-    lblDefs = new Set();
-    data.forEach(function(d){ 
-        var i;
-        var name1 = d[0].substring(0,i=d[0].indexOf("-"));
-        var name2 = d[0].substring(i+1);
-        lblDefs.add(name1);
-        lblDefs.add(name2);
-      });
-    lblDefs = Array.from(lblDefs);
-  }
+  lblDefs = new Set();
+  data.forEach(function(d){ 
+    var i;
+    d.name1 = d.rawArr[0].substring(0,i=d.rawArr[0].indexOf("-"));
+    d.name2 = d.rawArr[0].substring(i+1);
+    lblDefs.add(d.name1);
+    lblDefs.add(d.name2);
+  });
+  lblDefs = Array.from(lblDefs);
 
   lblDefs.forEach(function(d){
-    find(d, {name:d});
+    find(d, {data:d});
   });
 
-  return map[""];
+  console.log(map[""]);
+
+  return map;
 
 }
 
-csvToLinks = function(data) {
-  var map = {},
+csvToLinks = function(data, treeVertices) {
   imports = [];
 
-  // Compute a map from name to node.
-  data.filter(function(d){ return d.length==1; }).forEach(function(d) {
-    map[d[0].substring(d[0].lastIndexOf(".")+1)] = d;
-  });
-  if(map.size==0){
-    data.forEach(function(d){ 
-      var i;
-      var name1 = d[0].substring(0,i=d[0].indexOf("-"));
-      var name2 = d[0].substring(i+1);
-      map[name1]=name1;
-      map[name2]=name2;
-    });
-  }
+  console.log(treeVertices);
 
   // For each import, construct a link from the source to target node.
-  data.filter(function(d){ return d.length>1; }).forEach(function(d) {
-    var maxIdx = d3.max(d.slice(1));
+  data.forEach(function(d) {
+    d.interactionTimes = d.rawArr
+      .slice(1)
+      .map(function(s){ return parseInt(s); });
+    var maxIdx = d3.max(d.interactionTimes);
     while(imports.length<=maxIdx) 
       imports.push([]);
 
-    var name1 = d[0].substring(0,d[0].indexOf("-"));
-    var name2 = d[0].substring(d[0].indexOf("-")+1);
-
-    for(var i=1;i<d.length;i++){
-      imports[d[i]].push({source:map[name1], target:map[name2]});
-    }
+    d.interactionTimes
+     .forEach(function(t){ 
+       imports[t].push({source:treeVertices[d.name1], target:treeVertices[d.name2]});
+     });
       
   });
 
+  console.log(imports);
   return imports;
 }
