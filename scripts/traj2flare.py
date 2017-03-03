30b0fb90dc29e21f1c2f9a91bf30797f90ad550f
Rasmus Fonseca <fonseca.rasmus@gmail.com>
2017-03-03 11:41:58 -0800

    Trajectory writer works

--- scripts/traj2flare.py
+++ scripts/traj2flare.py
@@ -23,8 +23,9 @@ found_uniprot_id = False
 for res in gpcrdb_naming.residue_labels:
   if res[0]==uniprot_id:
     found_uniprot_id = True
-    resi_to_group[res[1]] = res[3]
-    resi_to_name[res[1]] = res[4]
+    cname = res[4]
+    resi_to_group[res[1]] = res[3] # cname[:cname.find(".")]
+    resi_to_name[res[1]] = cname[:cname.find(".")]+cname[cname.find("x"):]
 
 if not found_uniprot_id:
   print("ERROR: uniprot id '%s' wasn't found in GPCRdb naming database"%uniprot_id)
@@ -39,7 +40,7 @@ print("Analyzing hbond network in %d frames .."%len(t))
 
 hbond_frames = defaultdict(set)
 
-for f,frame in enumerate(t[:4]):
+for f,frame in enumerate(t[:20]):
   hbonds = md.baker_hubbard(frame, periodic=True)
   print("Frame %d .. %d hbonds"%(f,hbonds.shape[0]))
   for hbond in hbonds:
@@ -65,8 +66,8 @@ for resi1,resi2 in hbond_frames:
   framelist = sorted(list(hbond_frames[(resi1,resi2)]))
   edge_entries.append("    {\"name1\":\"%s\", \"name2\":\"%s\", \"frames\":%s}"%(resn1,resn2,str(framelist)))
 
-  tree_paths.add(resn1)
-  tree_paths.add(resn2)
+  tree_paths.add(resi_to_group[resi1]+"."+resn1)
+  tree_paths.add(resi_to_group[resi2]+"."+resn2)
 
 
 #Collect entries for tracks (coloring of nodes)
@@ -74,8 +75,9 @@ track_entries = []
 helix_colors = ["#1500D6","#003D97","#00E600","#00E600","#FEE200","#FF9000","#FF3B00","#FF0000"]
 for tp in tree_paths:
   try:
+    #res_name = tp[tp.rfind("x")+1:]
     res_name = tp[tp.rfind(".")+1:]
-    res_helix = int(tp[:tp.find(".")])
+    res_helix = int(tp[tp.rfind(".")+1:tp.find("x")])
     track_entries.append("      { \"nodeName\": \"%s\", \"color\": \"%s\", \"size\":\"1.0\" }"%(res_name,helix_colors[res_helix]))
   except ValueError: pass
   except IndexError: pass
@@ -89,7 +91,7 @@ with open(out_file,"w") as of:
   of.write("\n")
   of.write("  ],\n")
   of.write("  \"trees\": [\n")
-  of.write("    {,\n")
+  of.write("    {\n")
   of.write("      \"treeLabel\":\"Helices\",\n")
   of.write("      \"treePaths\": [\n")
   of.write(",\n".join(["        \""+tp+"\"" for tp in tree_paths]))
@@ -98,11 +100,12 @@ with open(out_file,"w") as of:
   of.write("    }\n")
   of.write("  ],\n")
   of.write("  \"tracks\": [\n")
+  of.write("    {\n")
   of.write("    \"trackLabel\": \"Helices\",\n")
   of.write("    \"trackProperties\": [\n")
   of.write(",\n".join(track_entries))
   of.write("\n")
-  of.write("    ]\n")
+  of.write("    ]}\n")
   of.write("  ],\n")
   of.write("  \"defaults\":{\"edgeColor\":\"rgba(50,50,50,100)\", \"edgeWidth\":2 }\n")
   of.write("}\n")
